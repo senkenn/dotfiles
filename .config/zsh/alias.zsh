f32ff818e7122a17779195e44023370e8d76db46
senkenn <senken32@gmail.com>
2025-02-08 14:51:36 +0900

    feat: add trash-cli installation script and update Zsh aliases for Docker and trash-cli

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,6 +1,11 @@
 alias ls="ls --color=auto"
 alias ll="ls -Al"
+
+# docker
 alias d=docker
 alias dc="docker compose"
 alias de='(){docker compose exec $1 bash}'
 alias dp="docker ps --format 'table {{.Names}}\t{{.Status}}'"
+
+# trash-cli
+alias rm="trash-put"
