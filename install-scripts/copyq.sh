db4ee0a1e4e89014bee6534ba8f2697fe8f8d921
SENKEN <65453007+senkenn@users.noreply.github.com>
2025-02-10 11:33:04 +0900

    Update copyq.sh

--- install-scripts/copyq.sh
+++ install-scripts/copyq.sh
@@ -4,5 +4,6 @@ sudo apt install -y copyq
 
 # copyq is not working by default: https://github.com/hluk/CopyQ/issues/2889
 # workaround: https://itsfoss.community/t/solved-copyq-not-saving-clipboard-copy-paste-in-ubuntu/10829/2
-cp ~/.config/autostart/copyq.desktop ~/.config/autostart/copyq.desktop~
-sed -i 's@Exec="/usr/bin/copyq"@Exec=env QT_QPA_PLATFORM=xcb copyq@' ~/.config/autostart/copyq.desktop
+# Execute the following commands after checking the "Autostart" box
+# cp ~/.config/autostart/copyq.desktop ~/.config/autostart/copyq.desktop~
+# sed -i 's@Exec="/usr/bin/copyq"@Exec=env QT_QPA_PLATFORM=xcb copyq@' ~/.config/autostart/copyq.desktop
