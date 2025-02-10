302045ea0b8eeb88423479e3d4cb196f9ca77d52
SENKEN <65453007+senkenn@users.noreply.github.com>
2025-02-10 11:29:58 +0900

    Update git.sh

--- install-scripts/git.sh
+++ install-scripts/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git config --global user.name "senkenn"
-git config --global user.email "senken32@gmail.com"
+# git config --global user.name "senkenn"
+# git config --global user.email "senken32@gmail.com"
 git config --global init.defaultBranch main
 git config --global safe.directory "*"
