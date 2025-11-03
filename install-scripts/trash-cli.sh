5041509ab2fa426bbf02f4a7286e7834b4982d41
senkenn <senken32@gmail.com>
2025-11-03 14:21:57 +0900

    update related prompt

--- install-scripts/trash-cli.sh
+++ install-scripts/trash-cli.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 sudo apt install -y trash-cli
+
+# aliases
+cat <<'EOF' >> ~/.zshrc
+
+# trash-cli
+alias rm="trash-put"
+EOF
--- prompt-installing/prezto.zsh
+++ prompt-installing/prezto.zsh
@@ -6,4 +6,3 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 echo "zstyle ':prezto:module:prompt' theme 'powerlevel10k'" >> $HOME/.zpreztorc
-echo "source $current_dir/.config/zsh/.p10k.zsh" >> $HOME/.zshrc
