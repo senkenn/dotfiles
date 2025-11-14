cf2738a5193c48abc3a626d8f20949b25546e135
senkenn <senken32@gmail.com>
2025-11-14 14:38:05 +0900

    fix: update pnpm installation script to set up zsh completion correctly

--- install-scripts/pnpm.sh
+++ install-scripts/pnpm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+set -euo pipefail
+
 curl -fsSL https://get.pnpm.io/install.sh | env SHELL=/bin/zsh sh -
-echo "source $HOME/.config/pnpm/pnpm-completion.zsh" >>"$HOME"/.zshrc
+
+# Set up pnpm completion for zsh
+mkdir -p ~/.config/pnpm
+pnpm completion zsh > ~/.config/pnpm/completion-for-pnpm.zsh
+cat << 'EOF' >> "$HOME/.zshrc"
+
+# pnpm completion
+source "$HOME/.config/pnpm/completion-for-pnpm.zsh"
+EOF
