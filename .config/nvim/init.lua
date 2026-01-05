6c398753d613407adc5c6f9854ccb09750d15198
senkenn <senken32@gmail.com>
2026-01-05 17:01:45 +0900

    feat: add editor settings and plugin lockfile for neovim
    Add tab indent, line numbers, project local config, and LSP diagnostics
    highlight settings. Include lazy-lock.json to pin plugin versions.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,17 @@
+-- タブインデント設定
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+-- 行番号表示
+vim.opt.number = true
+
+-- プロジェクトローカル設定を許可
+vim.opt.exrc = true
+
+-- LSP診断ハイライト無効
+vim.g.lsp_diagnostics_highlights_enabled = 0
+
 -- lazy.nvim bootstrap
 vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 
