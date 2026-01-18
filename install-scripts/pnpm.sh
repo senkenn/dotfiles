6b54f834d7fe57739479b869e62618d3bdb3cc28
senkenn <senken32@gmail.com>
2026-01-18 18:15:20 +0900

    chore: replace volta with standalone pnpm installation

--- install-scripts/pnpm.sh
+++ install-scripts/pnpm.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-volta install pnpm
+curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 # Set up pnpm completion for zsh
 mkdir -p ~/.config/pnpm
--- install-scripts/volta_0.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl https://get.volta.sh | bash
-echo "export PATH=\$HOME/.volta/bin:\$PATH" >>"$HOME/.zshrc"
